97560df982fb35f78ac0c432040375f877575304
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-07-11 15:38:30 +0200

    add safeguard - initialize ajax events only once with traffic-cop script

--- assets/js/traffic-cop.js
+++ assets/js/traffic-cop.js
@@ -1,25 +1,34 @@
-$(document).on('ajaxError', function (event, context, errorMsg, textStatus, jqXHR) {
-    if (context.handler === 'onSave' && jqXHR.status === 409) {
-        return false;
-    }
-});
+var initializedKey = 'kpolicar_backendtrafficcop_initialized';
 
-$(document).on('ajaxSetup', function (event, context) {
-    if (context.handler === 'onSave') {
-        context.options.update = {
-            'plugins/kpolicar/backendtrafficcop/layouts/retrievedat_formfield': '[js-retrieved-at]'
-        };
-    }
-});
+if ($('html').data(initializedKey) !== true) {
 
-$(document).on('ajaxFail', function (event, context, textStatus, jqXHR) {
-    if (context.handler === 'onSave' && jqXHR.status === 409) {
-        $.oc.confirm(jqXHR.responseText, function () {
-            $(event.target).request(context.handler, {
-                data: {
-                    _kpolicar_backendtrafficcop_confirmed: true
-                }
+    $(document).on('ajaxError', function (event, context, errorMsg, textStatus, jqXHR) {
+        if (context.handler === 'onSave' && jqXHR.status === 409) {
+            return false;
+        }
+    });
+
+    $(document).on('ajaxSetup', function (event, context) {
+        if (context.handler === 'onSave') {
+            context.options.update = $.extend(context.options.update, {
+                'plugins/kpolicar/backendtrafficcop/layouts/retrievedat_formfield': '[js-retrieved-at]'
             });
-        })
-    }
-});
+        }
+    });
+
+    $(document).on('ajaxFail', function (event, context, textStatus, jqXHR) {
+        if (context.handler === 'onSave' && jqXHR.status === 409) {
+            $.oc.confirm(jqXHR.responseText, function () {
+                $(event.target).request(context.handler, {
+                    data: {
+                        _kpolicar_backendtrafficcop_confirmed: true
+                    }
+                });
+            })
+        }
+    });
+
+    $('html').data(initializedKey, true)
+
+}
+
